b9a21dfdd1a63146ab723b3a769b6adda15e258f
Vageesh Hoskere <vageesh.hoskere@getzephyr.com>
2018-02-26 16:32:12 +0530

    quartz clustering program v2

--- quartz/src/main/java/PrintScheduler.java
+++ quartz/src/main/java/PrintScheduler.java
@@ -1,19 +1,32 @@
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
 
+import java.io.*;
+import java.util.Properties;
+
 import static org.quartz.JobBuilder.newJob;
 import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 
 public class PrintScheduler {
 
 	private Scheduler scheduler;
-	public PrintScheduler() {
+	public PrintScheduler(String instanceId) {
 		try {
-			scheduler = StdSchedulerFactory.getDefaultScheduler();
+			Properties properties = loadProperties();
+			properties.put("org.quartz.scheduler.instanceId",instanceId);
+			scheduler = new StdSchedulerFactory(properties).getScheduler();
 			scheduler.start();
-		} catch (SchedulerException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	private Properties loadProperties() throws FileNotFoundException,IOException {
+		Properties properties = new Properties();
+		try (InputStream fis = PrintScheduler.class.getResourceAsStream("quartz.properties")) {
+			properties.load(fis);
+		}
+		return properties;
 	}
 
 	public void schedule() throws SchedulerException {
@@ -28,7 +41,7 @@ public class PrintScheduler {
 	}
 
 	public static void main(String[] args) {
-		PrintScheduler printScheduler = new PrintScheduler();
+		PrintScheduler printScheduler = new PrintScheduler(args[0]);
 		try {
 //			printScheduler.schedule();
 			Thread.sleep(60000l);
